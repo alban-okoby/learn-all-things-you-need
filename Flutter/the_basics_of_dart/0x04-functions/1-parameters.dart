9de3d80933e252e83317fbf4766ef6b3350baad4
CerveauPlus <77627170+alban-okoby@users.noreply.github.com>
2023-09-20 19:19:03 +0000

    Function xith params

---
+++ Flutter/the_basics_of_dart/0x04-functions/1-parameters.dart
@@ -0,0 +1,14 @@
+void main () {
+  /**
+ * Learn dart (function)
+ * @author [Alban Okoby](https://www.github.com/alban-okoby)
+ */
+
+  /**
+   * functionWithParams (function)
+  */
+  int functionWithParams(int x, int y) {
+   return (x + y);
+  }
+
+}
