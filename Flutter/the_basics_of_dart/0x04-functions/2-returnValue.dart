1ab0a59124dc2632cc084d934b9ae9d6b484d083
CerveauPlus <77627170+alban-okoby@users.noreply.github.com>
2023-09-20 19:24:29 +0000

    Function with return value

---
+++ Flutter/the_basics_of_dart/0x04-functions/2-returnValue.dart
@@ -0,0 +1,14 @@
+void main() {
+   /**
+ * Learn dart (function)
+ * @author [Alban Okoby](https://www.github.com/alban-okoby)
+ */
+
+  /**
+   * functionWithReturnValue (function)
+  */
+
+  String functionWithReturnValue(String a, String b) {
+    return ("$a + $b");
+  }
+}
