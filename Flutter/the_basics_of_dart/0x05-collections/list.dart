d2dff6fef07a17be3681d1a52b5d708526c733d1
CerveauPlus <77627170+alban-okoby@users.noreply.github.com>
2023-09-27 23:35:28 +0000

    List collection

--- Flutter/the_basics_of_dart/0x05-collections/list.dart
+++ Flutter/the_basics_of_dart/0x05-collections/list.dart
@@ -2,6 +2,24 @@ void main() {
 
   // Declare an integers list
   List<int> myList = [1, 2, 3, 4, 5];
+  
+  // Add one new value of list
+  myList.add(17);
+
+ // Let's declare a new list of integers
+  List<int> notes = [10, 08, 19, 03];
+
+  // Add many value of the list
+  myList.addAll(notes);
+
+  // Update one value of the list
+  myList.insert(2, 14);
+
+  // remove a value of the list
+  myList.removeAt(2);
+
+  // Remove the last value of the list
+  myList.removeLast();
 
   print(myList);
 }
