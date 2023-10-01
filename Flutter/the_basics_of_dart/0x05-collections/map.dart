f9a378c3946a753092c2520e9c331e030128dc26
CerveauPlus <77627170+alban-okoby@users.noreply.github.com>
2023-10-01 17:31:05 +0000

    A map collection

---
+++ Flutter/the_basics_of_dart/0x05-collections/map.dart
@@ -0,0 +1,22 @@
+void main () {
+  
+  // Declare a Map 
+  Map<String, int> cars = new Map<String, int>();
+
+  // Add a values to the Map
+  cars = { 
+    'Mercedes': 111000,
+    'BMW': 14000,
+    'Honda': 9300 
+  };
+
+
+  // print the cars keys
+  print(cars.keys); // (Mercedes, BMW, Honda)
+
+  // print the cars prices
+  print(cars.values); // (111000, 14000, 9300)
+
+  // Print the cars keys and values
+  print(cars); // {Mercedes: 111000, BMW: 14000, Honda: 9300}
+}
