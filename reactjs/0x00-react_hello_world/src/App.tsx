25f0e5143dcaa481bfbf94ae3420bcb4c119a46f
Alban Kedi <albanokoby225@gmail.com>
2024-01-05 18:49:30 +0000

    Recat hello world

---
+++ reactjs/0x00-react_hello_world/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello World 🙌</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
