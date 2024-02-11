11d83a599fffa3b016e8e6a58af552a93bf0a70e
Alban Kedi <albanokoby225@gmail.com>
2024-02-11 16:31:14 +0000

    Event listeners

---
+++ vuejs/tutorial/0x01-declarative-rendering/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  // The reactive state goes here
+  data() {
+    return {
+      message: 'Declarative Rendering',
+      titleClass: 'title'
+    }
+  }
+ 
+}
+</script>
+
+<template>
+  <h1> {{ message }}  </h1> <br>
+  <button :class="titleClass"> {{ message }}  </button>
+</template>
---
+++ vuejs/tutorial/0x02-event-listeners/src/App.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  // The reactive state goes here
+  data() {
+    return {
+      message: "Event Listeners",
+      changeMe: "Change Me",
+      titleClass: "title",
+    };
+  },
+  methods: {
+    toggleColor() {
+      if (this.titleClass == "title") {
+        this.titleClass = "title-dark";
+      } else {
+        this.titleClass = "title";
+      }
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <br />
+  <button :class="titleClass" @click="toggleColor">{{ changeMe }}</button>
+</template>
+
+<style>
+  button {
+    color: white;
+    padding: 0.5rem 0.5rem;
+    border-radius: .5rem;
+    outline: none;
+  }
+  .title {
+    background-color: red;
+  }
+  .title-dark {
+    background-color: blue;
+  }
+</style>
