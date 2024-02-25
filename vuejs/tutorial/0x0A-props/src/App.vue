0f9dbf378507d4b4b257df261a463be13ec024cc
Alban Kedi <albanokoby225@gmail.com>
2024-02-25 18:51:38 +0000

    Props

---
+++ vuejs/tutorial/0x0A-props/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import Child from './components/child.vue'
+
+export default {
+  components: {
+    Child
+  },
+  data() {
+    return {
+      greeting: 'Hello du parent'
+    }
+  },
+  props: {
+    msg: String
+  }
+
+}
+</script>
+
+<template>
+  <Child :msg="msg"/>
+</template>
---
+++ vuejs/tutorial/0x0A-props/src/components/child.vue
@@ -0,0 +1,11 @@
+<script>
+    export default {
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<template>
+    <h3>I'm a child component.</h3>
+</template>
