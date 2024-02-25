e336f892351b50cf4d7280dde4e47b0abad08db4
Alban Kedi <albanokoby225@gmail.com>
2024-02-25 14:18:20 +0000

    life cycle and template refs

---
+++ vuejs/tutorial/0x07-lifecycle_and_template_refs/src/App.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  mounted() {
+    this.$refs.pElementRef.textContent = 'monté avec succès'
+  },
+}
+</script>
+
+<template>
+  <p ref="pElementRef">hello</p>
+</template>
