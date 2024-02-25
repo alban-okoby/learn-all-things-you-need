f8392e9e66895f372c4ddc791799865dde30868c
Alban Kedi <albanokoby225@gmail.com>
2024-02-25 17:16:37 +0000

    Components - child

---
+++ vuejs/tutorial/0x09-components/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import Child from './components/child.vue'
+
+export default {
+  components: {
+    Child
+  }
+}
+</script>
+
+<template>
+  <Child /> 
+  <p>I'm parent élément</p>
+</template>
+
+
+<style>
+  .container {
+  }
+</style>
---
+++ vuejs/tutorial/0x09-components/src/components/child.vue
@@ -0,0 +1,9 @@
+<script>
+    export default {
+
+    }
+</script>
+
+<template>
+    <h3>I'm a child component.</h3>
+</template>
