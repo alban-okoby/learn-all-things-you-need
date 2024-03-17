436ba137e136da918cefdac6f4ffd9a0e3a228ce
Alban Kedi <albanokoby225@gmail.com>
2024-03-17 02:50:24 +0000

    Slots

---
+++ vuejs/tutorial/0x0C-slots/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import Child from './components/child.vue'
+
+export default {
+  components: {
+    Child
+  },
+  data() {
+    return {
+      childMsg: 'Hello depuis le parent'
+    }
+  },
+
+}
+</script>
+
+<template>
+  <!-- <h2> Composant parent </h2> -->
+  <Child> Message: {{ childMsg }}  </Child>
+</template>
---
+++ vuejs/tutorial/0x0C-slots/src/components/child.vue
@@ -0,0 +1,3 @@
+<template>
+  <slot> Contenu de secours</slot>
+</template>  
