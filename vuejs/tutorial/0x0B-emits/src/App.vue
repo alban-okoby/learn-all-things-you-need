3eb52c673f6ed5200a760538498b139e0ec6a1bc
Alban Kedi <albanokoby225@gmail.com>
2024-03-17 02:09:59 +0000

    Emits

---
+++ vuejs/tutorial/0x0B-emits/src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import Child from './components/child.vue'
+
+export default {
+  components: {
+    Child
+  },
+  data() {
+    return {
+      childMsg: 'Hello du parent'
+    }
+  },
+
+}
+</script>
+
+<template>
+  <h2> Composant parent </h2>
+  <Child @response="(msg) => childMsg = msg"/>
+  <p> {{childMsg }} </p>
+</template>
---
+++ vuejs/tutorial/0x0B-emits/src/components/child.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  // Déclaration des évènement
+  emits: ["response"],
+  created() {
+    // emit Arg
+    this.$emit("response", "Message du composant enfant");
+  },
+};
+</script>
