b3c7f1a5e04b46131d37c91121ace92c6b13455d
Alban Kedi <albanokoby225@gmail.com>
2024-04-01 16:33:13 +0000

    Node URL

---
+++ nodejs/00-get-started/13-url.js
@@ -0,0 +1,22 @@
+const url = require('url');
+
+const myUrl = new URL('http://mywebsite.com/hello.html?id=100&status=active');
+
+// Serialized URL
+console.log(myUrl.href);
+console.log(myUrl.toString());
+
+// Host (root domain)
+console.log(myUrl.hostname);
+
+// Pathname
+console.log(myUrl.pathname);
+
+// Serialized query
+console.log(myUrl.search);
+
+// Params object
+console.log(myUrl.searchParams);
+
+// Add params
+console.log(myUrl.searchParams.append('name', 'John'));
