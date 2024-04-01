617c78e6fffd311b3505a9bf88d217de01b00f88
Alban Kedi <albanokoby225@gmail.com>
2024-04-01 11:00:15 +0000

    Description

--- nodejs/00-get-started/app.js
+++ nodejs/00-get-started/app.js
@@ -1,6 +1,25 @@
+const http = require('http');
 
-const _ = require('lodash');
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('Welcome to our home page');
+    }
+    if (req.url === '/about') {
+        // Blocking code
+        for (let i = 0; i < 1000; i++) {
+            for (let j = 0; j < 1000; j++) {
+                console.log(`${i} ${j}`);
+            }
+        }
+        res.end('Here is our short history');
+    }
+    // res.end(`
+    // <h1>Oops!</h1>
+    // <p>We can't seem to find the page you are looking for</p>
+    // <a href="/">back home</a>
+    // `)
+});
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flatMapDeep(items);
-console.log(newItems)
+server.listen(5000, () => {
+    console.log('Server listening on port : 5000...');
+});
