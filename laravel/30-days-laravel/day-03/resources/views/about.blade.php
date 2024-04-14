18bb10c3a083f54c6a95f063ec64dbbbf6574692
Alban Kedi <albanokoby225@gmail.com>
2024-04-14 10:43:13 +0000

    Day 3, layout

---
+++ laravel/30-days-laravel/day-03/resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+<h1>About Page </h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime sed esse unde expedita, delectus, voluptas non nesciunt enim consequatur consequuntur doloremque odio dignissimos beatae. Illo est fugit voluptas autem consequatur!</p>
+</x-layout>
