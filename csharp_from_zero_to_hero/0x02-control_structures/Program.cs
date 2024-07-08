78b82423a90321b3a28a05dcf8e35f27b2c8f4a9
Alban Kedi <albanokoby225@gmail.com>
2024-07-08 17:28:26 +0000

    If, else, switch statement

--- csharp_from_zero_to_hero/0x02-control_structures/Program.cs
+++ csharp_from_zero_to_hero/0x02-control_structures/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConditionalStatementsAndLoops {
+    class Program
+    {
+        static void Main(string[] args) {
+            int number = 7;
+
+            if (number > 10)
+            {
+                Console.WriteLine("The number is greater than 10.");
+            }
+            else if (number > 5)
+            {
+                Console.WriteLine("The number is greater than 5 but less than or equal to 10.");
+            }
+            else
+            {
+                Console.WriteLine("The number is 5 or less.");
+            }
+
+            // Switch Statement
+            int day = 3;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid day");
+                    break;
+            }        
+        }
+    }
+}
+ 
