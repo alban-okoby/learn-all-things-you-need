e8c6cff08a0d85397aebbdd2fe76ac352b0c844f
Alban Kedi <albanokoby225@gmail.com>
2024-07-08 17:56:46 +0000

    Loops c#

---
+++ csharp_from_zero_to_hero/0x03-control_loops/0x03-control_loops/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConditionalStatementsAndLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // For loop
+            for (int i = 0; i < 3; i++) {
+                Console.WriteLine("For loop interaction n = " + i);
+            }
+
+            // WHile Loop
+            int j = 0;
+            while (j < 3)
+            {
+                Console.WriteLine("While Loop interaction n = " + j);
+                j++;
+            }
+
+            // Do-While Loop
+            short k = 0;
+            do
+            {
+                Console.WriteLine("Do-While Loop interaction n = " + k);
+                k++;
+            } while (k < 3);
+        }
+    }
+}
