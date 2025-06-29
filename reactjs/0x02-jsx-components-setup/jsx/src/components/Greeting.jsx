03a6e8fb7b0833fb2970beb0ff6923b3953c772a
Alban Kedi <albanokoby225@gmail.com>
2025-06-29 07:18:33 +0000

    JSX and Components

---
+++ reactjs/0x02-jsx-components-setup/jsx/src/components/Greeting.jsx
@@ -0,0 +1,11 @@
+function Greeting() {
+  const user = 'Alban' 
+  return (
+    <>
+      <h1>Hello {user}</h1>
+      <p>Welcome to your React journey</p>
+    </>
+  )
+}
+
+export default Greeting;
